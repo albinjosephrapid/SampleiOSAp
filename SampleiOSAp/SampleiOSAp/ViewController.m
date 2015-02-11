85e42bd561897c877f0cba073f047b5131d32bde
Albin Joseph <albinjoseph@rapidvaluesolutions.com>
2015-02-11 09:13:39 +0530

    modified:   SampleiOSAp/SampleiOSAp/ViewController.m

--- SampleiOSAp/SampleiOSAp/ViewController.m
+++ SampleiOSAp/SampleiOSAp/ViewController.m
@@ -19,6 +19,7 @@
     // Do any additional setup after loading the view, typically from a nib.
     
     NSLog(@"Hello world");
+    NSLog(@"Changes in project");
 }
 
 - (void)didReceiveMemoryWarning {
